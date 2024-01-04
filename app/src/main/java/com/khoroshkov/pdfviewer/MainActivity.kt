aa476df4857c9a5c93ef1cc64738355a4c766eaa
KirillKhoroshkov <khoroshkovkirill@gmail.com>
2024-01-04 16:01:44 +0300

    Enabled edge-to-edge

--- app/src/main/java/com/khoroshkov/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/khoroshkov/pdfviewer/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.khoroshkov.pdfviewer
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.mutableStateOf
 import com.khoroshkov.pdfviewer.fragment.pdfdocumentviewer.PdfDocumentViewerFragment
 import com.khoroshkov.pdfviewer.ui.theme.PdfViewerTheme
@@ -17,6 +21,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val navigationBarStyle = when {
+                isSystemInDarkTheme() -> SystemBarStyle.dark(Color.TRANSPARENT)
+                else -> SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+            }
+
+            enableEdgeToEdge(navigationBarStyle = navigationBarStyle)
+
             PdfViewerTheme {
                 PdfDocumentViewerFragment(pdfUriState)
             }
@@ -27,14 +38,8 @@ class MainActivity : ComponentActivity() {
 
     private fun handleIntentIfNeeded(currentIntent: Intent) {
         val pdfUri = when (currentIntent.action) {
-            Intent.ACTION_VIEW -> {
-                println("DDDDDDDDDDDDDDDD ACTION_VIEW") // TODO delete
-                currentIntent.data
-            }
-            Intent.ACTION_SEND -> {
-                println("DDDDDDDDDDDDDDDD ACTION_SEND") // TODO delete
-                currentIntent.getParcelableExtra(Intent.EXTRA_STREAM)
-            }
+            Intent.ACTION_VIEW -> currentIntent.data
+            Intent.ACTION_SEND -> currentIntent.getParcelableExtra(Intent.EXTRA_STREAM)
             else -> null
         }
 
